06a142c9540b8a1c7e24f13e550b9bc791ba017f
Abel van Esseveldt <abelvess@gmail.com>
2020-02-18 15:30:24 +0100

    can add tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to task_path(@task)
   end
 
   def edit
@@ -32,5 +33,11 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
+  end
+
+    def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details)
   end
 end

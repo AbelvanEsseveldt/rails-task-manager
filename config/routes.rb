bf6af6a4c27426e8cbd44fa1d3323c692fae3ff2
Abel van Esseveldt <abelvess@gmail.com>
2020-02-18 16:16:00 +0100

    delete is working

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get "tasks/:id", to: "tasks#show", as: :task
   get 'tasks/:id/edit', to: "tasks#edit", as: :edit_task
   patch 'tasks/:id', to: "tasks#update", as: :update
-  delete 'tasks/:id', to: 'tasks#destroy'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
